5b79d7fd97d4ef774e68cb5c1c4fd721c5508e63
Scott Vanderlind <scottv@ifixit.com>
2015-01-15 08:19:59 -0800

    Update EchoDispatch example automation script
    EchoDispatch sits and listens for new commands through the TODO
    API. It iterates over them, prints them, and deletes them.
    This is a good starting point for a polling automation system.
    Replace the print command with anything you like. Regex against
    the command and so some cool stuff!

--- EchoDispatch.py
+++ EchoDispatch.py
@@ -1,19 +1,32 @@
-# A test script for PyEcho
-# By Scott Vanderlind, January 3 2015
+# A dispatch script for PyEcho. This script triggers events
+# based on commands that are spoken to Alexa.
+# By Scott Vanderlind, January 15 2015
 
 import PyEcho, getpass, time
 
+# Create an Echo object
 email = raw_input("Email: ")
 password = getpass.getpass()
 echo = PyEcho.PyEcho(email, password)
 
+# If we successfully logged in, loop and wait for events.
 if echo:
    while True:
+      # Get a list of tasks...
       tasks = echo.tasks()
+      # For every task in the list...
       for task in tasks:
-         print "NEW TODO: " + task['text']
+         # Parse the task, do some event.
+         # Here is where you might do a regex against the task text to
+         # make a decision on what to do.
+         command = task['text']
+         print "NEW COMMAND: " + command
+
+         # When we're done, delete it.
          res = echo.deleteTask(task)
          if res.status_code == 200:
             print "Task completed and deleted"
+
+      # Sleep a bit before checking for new events.
       time.sleep(5)
 
